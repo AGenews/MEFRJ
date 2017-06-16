6287396fcc5d3dac27fadf08079c3357a66d69d7
Andreas Genewsky <genewsky@web.de>
2017-06-16 07:43:33 +0200

    Update opencommutator.ino

--- opencommutator.ino
+++ opencommutator.ino
@@ -1,4 +1,4 @@
- //    OpenCommutator Control Software
+//    OpenCommutator Control Software
 //    Copyright (C) 2015 Andreas Genewsky
 //
 //    This program is free software: you can redistribute it and/or modify
@@ -15,8 +15,7 @@
 //    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 //    This sketch is inspired by Josh Siegle the OpenEphys community and
-//    the Twister sketch. Thanks!		
-
+//    the Twister sketch. Thanks!    
 
 #include <math.h>
 
@@ -31,7 +30,7 @@ int verPCB = 2;
 // -------------------------------------------------------------------- //
 //    DEBUG MODE?
  
-boolean debugMode = false;
+boolean debugMode = true;
 boolean fullspeed_noise_test = false;
 
 
@@ -40,17 +39,17 @@ boolean fullspeed_noise_test = false;
 // ARDUINO CONNECTIONS:
 //
 // DIGITAL I/O
-// D2	External Stop
-// D4	Indicator LED
-// D6	DIRECTION +
-// D7	ENABLE
-// D9	DIRECTION -
+// D2 External Stop
+// D4 Indicator LED
+// D6 DIRECTION +
+// D7 ENABLE
+// D9 DIRECTION -
 // D12  goes HIGH if Motor is on!
-// D13	on-board LED
+// D13  on-board LED
 //
 // ANALOG I/O
-// A0	SENS_VAL
-// A1	SPEED_VAL
+// A0 SENS_VAL
+// A1 SPEED_VAL
 
 
 // ARDUINO DEFINITIONS:
@@ -72,6 +71,7 @@ int maxspeed = 0;
 int speed = 0;
 int step = 5;
 int direction = 0;
+unsigned long task0 = 0;
 unsigned long task1 = 0;
 unsigned long task2 = 0;
 unsigned long  turning_start = 0;
@@ -84,136 +84,142 @@ int ledstate1 = HIGH;
 
 void setup()
 {
-	pinMode(extstop, INPUT_PULLUP);
-	pinMode(indled, OUTPUT);
-	pinMode(dirpos, OUTPUT);
-	pinMode(dirneg, OUTPUT);
-	pinMode(enable, OUTPUT);
-	pinMode(obled, OUTPUT);
-	pinMode(motoron, OUTPUT);
+  pinMode(extstop, INPUT_PULLUP);
+  pinMode(indled, OUTPUT);
+  pinMode(dirpos, OUTPUT);
+  pinMode(dirneg, OUTPUT);
+  pinMode(enable, OUTPUT);
+  pinMode(obled, OUTPUT);
+  pinMode(motoron, OUTPUT);
+  pinMode(A0,OUTPUT); 
+  digitalWrite(A0,HIGH);
 
-	if (debugMode) {
-    	Serial.begin(9600);
-  	}
-	extstate = digitalRead(extstop);
-	if(extstate == LOW){
-		attachInterrupt(0,start,RISING);
-	}
-	if(extstate == HIGH){
-		attachInterrupt(0,stop,LOW);
-	}
-	digitalWrite(indled, extstate);
+  if (debugMode) {
+      Serial.begin(9600);
+    }
+  extstate = digitalRead(extstop);
+  if(extstate == LOW){
+    attachInterrupt(0,start,RISING);
+  }
+  if(extstate == HIGH){
+    attachInterrupt(0,stop,LOW);
+  }
+  digitalWrite(indled, extstate);
 }
 
 
 void loop()
 {
-	if(fullspeed_noise_test){
-		turn(-255);
-		delay(1000);
-		turn(0);
-		delay(2000);
-		turn(255);
-		delay(1000);
-		turn(0);
-		delay(2000);
-	}
-	
-	
-	if(!fullspeed_noise_test){
-		if(!turning){turning_start = millis();}
-		if( ((millis()-turning_start)>5000)&&extstate==HIGH ){
-			fail=true;
-		}
-		sensval = get_sens();
-		maxspeed = get_speed();
-		
-		if( ((millis()-task1)>20)&&(!fail) ){
-			if(sensval<3.0){
-				speed += step;
-				direction = 1;
-			}
-			if(sensval>97.0){
-				speed += step;
-				direction = -1;
-			}
-			if( (sensval>5.0) && (sensval<95.0) ){
-				speed = minspeed;
-				direction = 0;
-			}
-			speed = constrain(speed,minspeed,maxspeed);
-			turn(speed*direction);
-			task1 = millis();
-		}
-		if(fail){
-			turn(0);
-			if((millis()-task2)>150){
-				ledstate1 = !ledstate1;
-				digitalWrite(indled, ledstate1);
-				task2 = millis();
-			}
-		}
-	}
+  if(fullspeed_noise_test){
+    turn(-255);
+    delay(1000);
+    turn(0);
+    delay(2000);
+    turn(255);
+    delay(1000);
+    turn(0);
+    delay(2000);
+  }
+
+
+  if((millis()-task0)>20){
+    pinMode(A0,INPUT); 
+    if((millis()-task0)>25){
+      sensval = analogRead(senspin);
+      sensval = map(sensval,12,478,0,100);
+      pinMode(A0,OUTPUT);
+      digitalWrite(A0,HIGH);
+      task0 = millis();
+      Serial.println(sensval);
+    }
+  }
+    
+  if(!fullspeed_noise_test){
+    if(!turning){turning_start = millis();}
+    if( ((millis()-turning_start)>5000)&&extstate==HIGH ){
+      fail=true;
+    }
+    
+    maxspeed = get_speed();
+    
+    if( ((millis()-task1)>20)&&(!fail) ){
+      if(sensval<3.0){
+        speed += step;
+        direction = 1;
+      }
+      if(sensval>97.0){
+        speed += step;
+        direction = -1;
+      }
+      if( (sensval>5.0) && (sensval<95.0) ){
+        speed = minspeed;
+        direction = 0;
+      }
+      speed = constrain(speed,minspeed,maxspeed);
+      turn(speed*direction);
+      task1 = millis();
+    }
+    if(fail){
+      turn(0);
+      if((millis()-task2)>150){
+        ledstate1 = !ledstate1;
+        digitalWrite(indled, ledstate1);
+        task2 = millis();
+      }
+    }
+  }
 }
 
 
 void start(){
-	extstate = HIGH;
-	attachInterrupt(0,stop,LOW);
-	digitalWrite(indled, extstate);
+  extstate = HIGH;
+  attachInterrupt(0,stop,LOW);
+  digitalWrite(indled, extstate);
 }
 void stop(){
-	extstate = LOW;
-	attachInterrupt(0,start,RISING);
-	digitalWrite(indled, extstate);
-	digitalWrite(enable, LOW);
-	digitalWrite(dirpos, LOW);
-	digitalWrite(dirneg, LOW);
-	fail=false;
-	
+  extstate = LOW;
+  attachInterrupt(0,start,RISING);
+  digitalWrite(indled, extstate);
+  digitalWrite(enable, LOW);
+  digitalWrite(dirpos, LOW);
+  digitalWrite(dirneg, LOW);
+  fail=false;
+  
 }
 
 void turn(int speed){
-	int val = 0;
-	if(speed<0){
-		val = abs(speed);
-		digitalWrite(enable, extstate);
-		analogWrite(dirpos, 0);
-		analogWrite(dirneg, val);
-		digitalWrite(obled,HIGH);
-		digitalWrite(motoron,HIGH);
-		turning = true;
-	}
-	if(speed==0){
-		digitalWrite(enable, LOW);
-		analogWrite(dirpos, 0);
-		analogWrite(dirneg, 0);
-		digitalWrite(obled, LOW);
-		digitalWrite(motoron,LOW);
-		turning = false;
-	}
-	if(speed>0){
-		val = abs(speed);
-		digitalWrite(enable, extstate);
-		analogWrite(dirpos, val);
-		analogWrite(dirneg, 0);
-		digitalWrite(obled,HIGH);
-		digitalWrite(motoron,HIGH);
-		turning = true;
-	}	
+  int val = 0;
+  if(speed<0){
+    val = abs(speed);
+    digitalWrite(enable, extstate);
+    analogWrite(dirpos, 0);
+    analogWrite(dirneg, val);
+    digitalWrite(obled,HIGH);
+    digitalWrite(motoron,HIGH);
+    turning = true;
+  }
+  if(speed==0){
+    digitalWrite(enable, LOW);
+    analogWrite(dirpos, 0);
+    analogWrite(dirneg, 0);
+    digitalWrite(obled, LOW);
+    digitalWrite(motoron,LOW);
+    turning = false;
+  }
+  if(speed>0){
+    val = abs(speed);
+    digitalWrite(enable, extstate);
+    analogWrite(dirpos, val);
+    analogWrite(dirneg, 0);
+    digitalWrite(obled,HIGH);
+    digitalWrite(motoron,HIGH);
+    turning = true;
+  } 
 }
 
-
-float get_sens(){
-	float val = 0;
-	val = analogRead(senspin);
-	val = val/10.23;
-	return val;
-}
 int get_speed(){
-	int val = 0;
-	val = analogRead(speedpin);
-	val = map(val,0,1023,0,255);
-	return val;
+  int val = 0;
+  val = analogRead(speedpin);
+  val = map(val,0,1023,0,255);
+  return val;
 }
-
